e42b84b01f66e3752a6fbbd737902e965e026b32
Vinícius <viniciusotonimsv@gmail.com>
2021-09-14 15:28:22 -0300

    ok

---
+++ reactjs/src/pages/components/cabecalho/index.js
@@ -0,0 +1,11 @@
+export default function Cabecalho() {
+    return (
+        <main> 
+            <div className="logo-cabecalho"> <img src="/assets/images/logo.svg" alt="" /> </div> 
+            <div className="titulo"> GameBud </div>
+            <div className="pesquisa"> <input placeholder="pesquise seu produto" /> </div>
+            <div className="Login"> Login </div>
+            <div className="carrinho"> <img src="/assets/images/carrinho.svg" alt="" /> </div>
+        </main>
+    )
+}
