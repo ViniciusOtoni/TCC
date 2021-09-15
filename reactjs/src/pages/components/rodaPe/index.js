e72f2f703dcd1bb7754907d51e2a6b99e77c92d6
luigi <luigidasilvacoelho@gmail.com>
2021-09-14 23:55:22 -0300

    ok

--- reactjs/src/pages/components/rodaPe/index.js
+++ reactjs/src/pages/components/rodaPe/index.js
@@ -7,7 +7,7 @@ export default function Footer(){
         <RodaPe>
             <div className="section1">
                 <div className="align-title">
-                    <img src="" alt="logo"/>
+                    <img className="title-image" src="/assets/images/logo.svg" alt="logo"/>
                     <div> GameBud </div>
                 </div>    
                 <div className="item-header"> Exemplo </div>
@@ -16,43 +16,53 @@ export default function Footer(){
                 <div className="item-header"> Exemplo </div>
             </div>
             <div className="section2">
-                <div className="align-itens">
-                    <div className="item"> Exemplo </div>
-                    <div className="item"> Exemplo </div>
-                    <div className="item"> Exemplo </div>
+                <div className="align-itens-image">
+                    <div className="item-image"> Exemplo </div>
+                    <div className="item-image"> Exemplo </div>
+                    <div className="item-image"> Exemplo </div>
                     <div className="item-image"> 
                         <img src="/assets/images/facebook.svg"/>
-                        <img src="/assets/images/twitter.svg"/>
                         <img src="/assets/images/instagram.svg"/>
+                        <img src="/assets/images/facebook.svg"/>
                     </div>
                 </div>
-                <div className="align-itens">
-                    <div className="item"> Exemplo </div>
-                    <div className="item"> Exemplo </div>
-                    <div className="item"> Exemplo </div>
-                    <div className="item"> Exemplo </div>
-                    <div className="item"> Exemplo </div>
-                    <div className="item"> Exemplo </div>
-                </div>
-                <div className="align-itens">
-                    <div className="item"> Exemplo </div>
-                    <div className="item"> Exemplo </div>
-                    <div className="item"> Exemplo </div>
-                    <div className="item"> Exemplo </div>
-                    <div className="item"> Exemplo </div>
-                    <div className="item"> Exemplo </div>
-                </div>
-                <div className="align-itens">
-                    <div className="item"> Exemplo </div>
-                    <div className="item"> Exemplo </div>
-                    <div className="item"> Exemplo </div>
-                    <div className="item"> Exemplo </div>
-                    <div className="item"> Exemplo </div>
-                    <div className="item"> Exemplo </div>
-                </div>
+                <div className="align">
+                    <div className="align-items1">
+                        <div className="item"> Exemplo </div>
+                        <div className="item"> Exemplo </div>
+                        <div className="item"> Exemplo </div>
+                        <div className="item"> Exemplo </div>
+                        <div className="item"> Exemplo </div>
+                        <div className="item"> Exemplo </div>
+                    </div>
+                    <div className="align-items">
+                        <div className="item"> Exemplo </div>
+                        <div className="item"> Exemplo </div>
+                        <div className="item"> Exemplo </div>
+                        <div className="item"> Exemplo </div>
+                        <div className="item"> Exemplo </div>
+                        <div className="item"> Exemplo </div>
+                    </div>
+                    <div className="align-items">
+                        <div className="item"> Exemplo </div>
+                        <div className="item"> Exemplo </div>
+                        <div className="item"> Exemplo </div>
+                        <div className="item"> Exemplo </div>
+                        <div className="item"> Exemplo </div>
+                        <div className="item"> Exemplo </div>
+                    </div>
+                    <div className="align-items4">
+                        <div className="item"> Exemplo </div>
+                        <div className="item"> Exemplo </div>
+                        <div className="item"> Exemplo </div>
+                        <div className="item"> Exemplo </div>
+                        <div className="item"> Exemplo </div>
+                        <div className="item"> Exemplo </div>
+                    </div>
+                </div>   
             </div>
-            <hr />
             <div className="section3">
+                <hr />
                 <div> Copyright© 1995-2021 SAMSUNG. Todos os direitos reservados.
 SAMSUNG ELETRÔNICA DA AMAZÔNIA LTDA., com sede em Av. dos Oitis, nº 1.460, Distrito Industrial, Manaus/AM, 69.007-002, inscrita no CNPJ/MF sob o nº. 00.280.273/0001-37.
 LOJA ONLINE SAMSUNG, operada pela SYNAPCOM COMERCIO ELETRÔNICO LTDA, com endereço na Avenida Antônio Cândido Machado, 3100, BOX 86 - Cajamar - SP, CEP: 07776-550, inscrita no CNPJ sob o nº. 27.932.734/0002-46 e IE 241.130.824.114. 
--- reactjs/src/pages/components/rodaPe/styled.js
+++ reactjs/src/pages/components/rodaPe/styled.js
@@ -3,6 +3,14 @@ import styled from "styled-components";
 const RodaPe = styled.div`
     display: flex;
     flex-direction: column;
+    margin: 0px;
+    body{
+        margin: 0px;
+    }
+
+    * {
+        box-sizing: border-box;
+    }
 
     main{
         display: flex;
@@ -12,35 +20,113 @@ const RodaPe = styled.div`
     .section1{
         display: flex;
         flex-direction: row;
-    }
+        justify-content: space-around;
 
-    .align-title{
-        display: flex;
-        flex-direction: row;
+        background-color: #DFA04E;
+
+        font-family: sans-serif;
+        font-weight: 800;
+
+        width: 100%;
+        height: 5em;
+        align-items: center;
     }
 
     .section2{
         display: flex;
         flex-direction: row;
-    }
+        justify-content: space-between;
 
-    .align-itens{
-        display: flex;
-        flex-direction: column;
-    }
+        background-color: #F0AC54;
 
-    .item-image{
-        padding: 1em 5em;
+        font-family: sans-serif;
+        font-weight: 500;
+
+        align-items: center;
+
+        width: 100%;
     }
 
     .item{
-        padding: 1em 6em;
+        margin: .8em 0em;
     }
 
-    .item-header{
-        padding: 1em 5em;
+    .item:hover{
+        cursor: pointer;
+        text-decoration: underline;
     }
 
+    .align{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+
+        margin-right: 3.5em;
+
+        width: 48em;
+    }
+
+    .align-title{
+        display: flex;
+        flex-direction: row;
+
+        justify-content: center;
+        align-items: center;
+
+        margin-right: 8em;
+    }
+
+    .title-image{
+        height: 5em;
+        width: 5em;
+    }
+
+
+    .align-itens-image{
+       display: flex;
+       flex-direction: column;
+       justify-content: center;
+       align-items: center;
+
+       margin-left: 8em;
+    }
+
+
+    .align-items4{
+        padding-right: 1.5em;
+    }
+
+
+    .item-image{
+        margin: .5em 0em;
+    }
+
+    .item-image:hover{
+        cursor: pointer;
+        text-decoration: underline;
+    }
+
+    hr{
+        border: 0.1px solid #333333;
+        width: 100%;
+    }
+
+    .section3{
+        background-color: #F0AC54;
+
+        font-family: sans-serif;
+        font-size: .9em;
+        line-height: 1.5em;
+
+        width: 100%;
+        height: 13em;
+    }
+
+    .section3 > div {
+        padding-left: 1.5em;
+        padding-top: 1.5em;
+        width: 80%;
+    }
 `
 
 export default  RodaPe 
