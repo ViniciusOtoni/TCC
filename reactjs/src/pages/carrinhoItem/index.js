50da45f5223b15b1551c4d88bbcce8778877d11b
Vinícius <viniciusotonimsv@gmail.com>
2021-09-15 15:41:17 -0300

    ok

--- reactjs/src/pages/carrinhoItem/index.js
+++ reactjs/src/pages/carrinhoItem/index.js
@@ -14,6 +14,20 @@ export default function CarrinhoItem() {
                         <div className="preco"> Preço </div>
                     </div>
                 </div>
+                <div className="box-preta">
+                    <div className="foto"> <img src="" alt="" /> </div>
+                    <div className="column-esp"> 
+                        <div className="nome-produto"> Nome Grande Para Testar O Width e Ver O Quanto Que Vai ser Pra Ficar Bom</div>
+                        <div className="row-vendido"> 
+                            <div className="dono"> Vendido Por: </div>
+                            <div className="nome-dono"> GameBud </div>
+                        </div>
+                        <div className="row-vendido"> 
+                            <div className="dono"> Entregue Por: </div>
+                            <div className="nome-dono"> GameBudSedex </div>
+                        </div>
+                    </div>
+                </div>
            <Footer />
        </StyledCarrinhoItem>
     )
