b0cfd9e350a598753ce26eaefc9f77de51027613
Vinícius <viniciusotonimsv@gmail.com>
2021-09-21 09:02:28 -0300

    ok

--- reactjs/src/components/popup/styled.js
+++ reactjs/src/components/popup/styled.js
@@ -4,6 +4,8 @@ const StyledPopup = styled.div`
 
 background-color: #333333;
 width: 400px;
+color: #ffffff;
+
 
 header {
     display: flex;
@@ -11,6 +13,22 @@ header {
     background-color: #000000;
     color: #ffffff;
     width: 400px;
+}
+
+.email {
+    font-family: MontserratBold;
+}
+
+.senha {
+    font-family: MontserratBold;
+}
+
+.esqueceu-email {
+    font-family: Medio;
+}
+
+.esqueceu-senha {
+    font-family: Medio;
 }
 `
 
