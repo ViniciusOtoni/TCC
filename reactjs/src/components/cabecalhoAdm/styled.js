e9e3f2f7abf2d4dc849324d9d2bf8dd7260d3dca
Vinícius <viniciusotonimsv@gmail.com>
2021-09-24 07:55:28 -0300

    ok

--- reactjs/src/components/cabecalhoAdm/styled.js
+++ reactjs/src/components/cabecalhoAdm/styled.js
@@ -6,80 +6,7 @@ const StyledCabecalhoAdm = styled.div `
 
 
 
-@media(max-width:375px) {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        margin: 0em;
-        padding: 0em;
-        width: 23em;
 
-        
-
-        .agp {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            margin: 0em;
-            padding: 0em;
-            margin-bottom: 1em;
-        }
-
-        
-        .input {
-           
-        }
-
-       
-
-        .agp-input {
-            
-            display: flex;
-            flex-direction: row;
-            justify-content: normal;
-        }
-
-        
-
-    .NV {
-        width: 86%;
-        margin-left: 2em;
-    }
-
-
-       
-
-        .PE {
-            width: 86% !important;
-            margin-left: 2em;
-        }
-
-        .in {
-            width: 20em !important;
-        }
-
-        
-
-        .pedidos {
-            width: 100%;
-        }
-
-
-        
-       
-
-        .column {
-            display: flex;
-            flex-direction: column;
-            margin: 0em;
-            padding: 0em;
-            width: 100%;
-        }
-
-     
-}
 
 
 
@@ -168,9 +95,67 @@ const StyledCabecalhoAdm = styled.div `
         margin-right: ${props => props.bNulo === 'true' ? '2em' : 'none'};
     }
 
-    .nv{
+    .nv {
         margin-right: ${props => props.bNulo === 'true' ? '2em' : 'none'};
     }
+
+    
+    @media(max-width:375px) {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        margin: 0em;
+        padding: 0em;
+        width: 23em;
+
+        background-color: red;
+
+
+        .agp {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin: 0em;
+            padding: 0em;
+            margin-bottom: 1em;
+        }
+        
+        .agp-input {
+            
+            display: flex;
+            flex-direction: row;
+            justify-content: normal;
+        }
+
+        .NV {
+            width: 86%;
+            margin-left: 2em;
+        }
+
+        .PE {
+            width: 86% !important;
+            margin-left: 2em;
+        }
+
+        .in {
+            width: 20em !important;
+        }
+
+        .pedidos {
+            width: 100%;
+        }
+
+        .column {
+            display: flex;
+            flex-direction: column;
+            margin: 0em;
+            padding: 0em;
+            width: 100%;
+        }
+    }
+
 
 
 ` 
