e19d2fe3bb23e2a71677cc74f4317c2b4b19e665
Lucas da Silva Farias <mi.ohta5@gmail.com>
2021-10-26 15:13:37 -0300

    organização de código

--- reactjs/src/components/botaoAdm/styled.js
+++ reactjs/src/components/botaoAdm/styled.js
@@ -6,18 +6,17 @@ const  StyledButtonAdm = styled.button`
     padding: .7em 1.5em;
 
     background-color: ${props => props.cor === 'vermelho' ? '#D9534F;' 
-                                                    : props.cor === 'laranja' ? '#F0AC54;' 
-                                                                             :  props.cor === 'preto' ? '#1A1A1A;'
-                                                                                : '#5CB85C;'
-                                                                              };
+                      : props.cor === 'laranja' ? '#F0AC54;' 
+                      :  props.cor === 'preto' ? '#1A1A1A;'                                                          
+                      : '#5CB85C;'};
     border: none;
     
     &:hover{
         cursor: pointer;
         background-color: ${props => props.cor === 'vermelho' ? '#b5433f' 
-                                                        : props.cor === 'laranja' ? '#cf9346;' 
-                                                                                  :  props.cor === 'preto' ? '#000000' 
-                                                                                    : '#50a150;'};
+                          : props.cor === 'laranja' ? '#cf9346;' 
+                          :  props.cor === 'preto' ? '#000000' 
+                          : '#50a150;'};
         transition: .5s;
     }
 `                                                                             
