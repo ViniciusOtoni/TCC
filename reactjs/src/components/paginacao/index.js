08730205f29003ac59960037df1fbda2994c3246
LucasFarias <mi.ohta5@gmail.com>
2021-10-26 08:58:58 -0300

    paginação

--- reactjs/src/components/paginacao/index.js
+++ reactjs/src/components/paginacao/index.js
@@ -1,15 +1,39 @@
 import { StyledPaginacao } from "./styled";
+import { useState} from "react";
+
+import Api from "../../services/api";
+const api = new Api();
 
 export default function Paginacao() {
+    const [page, setPage] = useState(1);
+
+    function skipPage() {
+        let pagina = page + 1;
+        console.log(page);
+        setPage(pagina);
+    }
+
+    function returnPage() {
+        let pagina = page - 1;
+        console.log(page);
+        setPage(pagina);
+    }
+
+    function listarPlus() {
+        skipPage();
+        const x = api.listarProdutos(page);
+        return(x);
+    }
+
     return (
         <StyledPaginacao>
             <main className="pc"> 
-                <div className="button-mudar"> <button> <img src="/assets/images/voltar.svg" alt="" /> Anterior  </button> </div>
+                <div className="button-mudar"> <button onClick={returnPage}> <img src="/assets/images/voltar.svg" alt="" /> Anterior  </button> </div>
                 <div className="numero-pagina"> 1 </div>
                 <div className="numero-pagina"> 2 </div>
                 <div className="numero-pagina"> 3 </div>
                 <div className="numero-pagina"> 4 </div>
-                <div className="button-mudar"> <button> Próximo <img src="/assets/images/avancar.svg" alt="" /> </button></div> 
+                <div className="button-mudar"> <button onClick={skipPage}> Próximo <img src="/assets/images/avancar.svg" alt="" /> </button></div> 
             </main>
 
             
@@ -20,8 +44,8 @@ export default function Paginacao() {
                 <div className="numero-pagina"> 3 </div>
             </div>
             <div className="row"> 
-                <div className="button-mudar"> <button> <img src="/assets/images/voltar.svg" alt="" /> Anterior  </button> </div>
-                <div className="button-mudar"> <button> Próximo <img src="/assets/images/avancar.svg" alt="" /> </button></div> 
+                <div className="button-mudar"> <button onClick={returnPage}> <img src="/assets/images/voltar.svg" alt="" /> Anterior  </button> </div>
+                <div className="button-mudar"> <button onClick={skipPage}> Próximo <img src="/assets/images/avancar.svg" alt="" /> </button></div> 
             </div>
             </main>
         </StyledPaginacao> 
