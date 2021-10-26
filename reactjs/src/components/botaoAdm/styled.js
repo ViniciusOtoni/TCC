ae4219e2e30c54af31229846665cbd6921c75cd0
Lucas da Silva Farias <mi.ohta5@gmail.com>
2021-10-26 15:21:39 -0300

    organizaaaaaaa

--- reactjs/src/components/botaoAdm/styled.js
+++ reactjs/src/components/botaoAdm/styled.js
@@ -1,23 +1,31 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-const  StyledButtonAdm = styled.button`
-    font-family: medio;
-    color: white;
-    padding: .7em 1.5em;
+const StyledButtonAdm = styled.button`
+  font-family: medio;
+  color: white;
+  padding: 0.7em 1.5em;
 
-    background-color: ${props => props.cor === 'vermelho' ? '#D9534F;' 
-                      : props.cor === 'laranja' ? '#F0AC54;' 
-                      :  props.cor === 'preto' ? '#1A1A1A;'                                                          
-                      : '#5CB85C;'};
-    border: none;
-    
-    &:hover{
-        cursor: pointer;
-        background-color: ${props => props.cor === 'vermelho' ? '#b5433f' 
-                          : props.cor === 'laranja' ? '#cf9346;' 
-                          :  props.cor === 'preto' ? '#000000' 
-                          : '#50a150;'};
-        transition: .5s;
-    }
-`                                                                             
-export { StyledButtonAdm }
+  background-color: ${(props) =>
+    props.cor === "vermelho"
+      ? "#D9534F;"
+      : props.cor === "laranja"
+      ? "#F0AC54;"
+      : props.cor === "preto"
+      ? "#1A1A1A;"
+      : "#5CB85C;"};
+  border: none;
+
+  &:hover {
+    cursor: pointer;
+    background-color: ${(props) =>
+      props.cor === "vermelho"
+        ? "#b5433f"
+        : props.cor === "laranja"
+        ? "#cf9346;"
+        : props.cor === "preto"
+        ? "#000000"
+        : "#50a150;"};
+    transition: 0.5s;
+  }
+`;
+export { StyledButtonAdm };
