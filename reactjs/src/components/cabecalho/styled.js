6a0226c36ad2d1a647a667679901f388fba2b918
LucasFarias <mi.ohta5@gmail.com>
2021-10-29 08:51:58 -0300

    organização de código v4.0

--- reactjs/src/components/cabecalho/styled.js
+++ reactjs/src/components/cabecalho/styled.js
@@ -1,246 +1,233 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-
-
-const StyledCabecalho = styled.div `
-   
-
-     .pc {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-        background-color: #333333;
-    
+const StyledCabecalho = styled.div`
+  .pc {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #333333;
 
     margin: auto;
-     & * {z-index : 11;}
-     }
-
-    .logo-cabecalho  img {
-        width: 6em;
-        height: 5em;
-        padding-top: 0.4em;
-        cursor: pointer;
+    & * {
+      z-index: 11;
     }
-    
-    .logo-cabecalho {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        margin-left: 3em;
-    }
+  }
 
-    .titulo {
-        font-family: MontserratBold;
-        font-size: 1.5em;
-        color: ${props => props.corLetra === 'branco' ? '#ffffff' : '#F0AC54'};
-        cursor: pointer;
-    }
+  .logo-cabecalho img {
+    width: 6em;
+    height: 5em;
+    padding-top: 0.4em;
+    cursor: pointer;
+  }
 
-    .pesquisa{
-        width: 30%;
-        margin-bottom: 1em;
-    }
+  .logo-cabecalho {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-left: 3em;
+  }
 
-    .pesquisa input {
-        display: ${props => props.corLetra === 'nulo'  ? 'none' : 'block' };
-        position: relative;
+  .titulo {
+    font-family: MontserratBold;
+    font-size: 1.5em;
+    color: ${(props) => (props.corLetra === "branco" ? "#ffffff" : "#F0AC54")};
+    cursor: pointer;
+  }
 
-        background-color: transparent;
-        font-family: MontserratBold; 
-        color: white;
-        
-        border-radius: 5em;
-        border: 2px solid #ffffff;
-      
-        width: 100%;
-        height: 2.5em;
+  .pesquisa {
+    width: 30%;
+    margin-bottom: 1em;
+  }
 
-        padding: 0em 2.5em;
-        
-        text-decoration: none;
-        outline: none;
-        cursor: text;
-    }
- 
-    .input::placeholder {
-        color: white;
-       
-        font-family: MontserratBold; 
-        
-    }
+  .pesquisa input {
+    display: ${(props) => (props.corLetra === "nulo" ? "none" : "block")};
+    position: relative;
 
-    .input:focus::-webkit-input-placeholder {
-        color: transparent;
-     }
+    background-color: transparent;
+    font-family: MontserratBold;
+    color: white;
 
-     .lupa {
-         display: ${props => props.corLetra === 'nulo'  ? 'none' : 'block' };
-         position: relative;
-         top: 1.5em;
-         left: .8em;
-         width: 1em;
-         height: 1em;
+    border-radius: 5em;
+    border: 2px solid #ffffff;
 
-         z-index: 12;
-     }
+    width: 100%;
+    height: 2.5em;
 
-    .login {
-        color: ${props => props.corLetra === 'branco' ? '#ffffff' : '#F0AC54'};
-        font-size: 1.5em;
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        position: relative;
-        font-family: MontserratBold;
-        display: ${props => props.corLetra === 'nulo'  ? 'none' : 'block' };
-       
-        cursor: pointer;
-    }
+    padding: 0em 2.5em;
 
-    .user-login {
-        color: ${props => props.corLetra === 'branco' ? '#ffffff' : '#F0AC54'};
-        display: ${props => props.corLetra === 'nulo'  ? 'none' : 'block' };
-        font-family: MontserratBold;
-        font-size: 1.5em;
-        width: 12em;
-      }
+    text-decoration: none;
+    outline: none;
+    cursor: text;
+  }
 
-    
-    .carrinho {
-        padding-top: 0.4em;
-        padding-left: 0em;
-        margin-right: 3.4em;
-        position: relative;
-        content: url(${props => props.corLetra === 'branco' ? '/assets/images/cBranco.svg' : '/assets/images/carrinho.svg'});
-        width: 4em;
-        height: 4em;
-        display: ${props => props.corLetra === 'nulo'  ? 'none' : 'block' };
-    
-       
-        cursor: pointer;
+  .input::placeholder {
+    color: white;
+
+    font-family: MontserratBold;
+  }
+
+  .input:focus::-webkit-input-placeholder {
+    color: transparent;
+  }
+
+  .lupa {
+    display: ${(props) => (props.corLetra === "nulo" ? "none" : "block")};
+    position: relative;
+    top: 1.5em;
+    left: 0.8em;
+    width: 1em;
+    height: 1em;
+
+    z-index: 12;
+  }
+
+  .login {
+    color: ${(props) => (props.corLetra === "branco" ? "#ffffff" : "#F0AC54")};
+    font-size: 1.5em;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    position: relative;
+    font-family: MontserratBold;
+    display: ${(props) => (props.corLetra === "nulo" ? "none" : "block")};
+
+    cursor: pointer;
+  }
+
+  .user-login {
+    color: ${(props) => (props.corLetra === "branco" ? "#ffffff" : "#F0AC54")};
+    display: ${(props) => (props.corLetra === "nulo" ? "none" : "block")};
+    font-family: MontserratBold;
+    font-size: 1.5em;
+  }
+
+  .carrinho {
+    padding-top: 0.4em;
+    padding-left: 0em;
+    margin-right: 3.4em;
+    position: relative;
+    content: url(${(props) =>
+      props.corLetra === "branco"
+        ? "/assets/images/cBranco.svg"
+        : "/assets/images/carrinho.svg"});
+    width: 4em;
+    height: 4em;
+    display: ${(props) => (props.corLetra === "nulo" ? "none" : "block")};
+
+    cursor: pointer;
+  }
+
+  .cell {
+    display: none;
+  }
+
+  .entrega {
+    color: #ffffff;
+    font-size: 1.5em;
+    font-family: MontserratBold;
+    display: ${(props) => (props.corLetra === "branco" ? "block" : "none")};
+    cursor: pointer;
+  }
+
+  hr {
+    margin-left: 3.4em;
+    margin-right: 3.4em;
+    border-color: ${(props) =>
+      props.corLetra === "nulo" ? "#F0AC54" : "#FFFFFF"};
+    display: ${(props) => (props.corLetra === "branco" ? "none" : "block")};
+  }
+
+  .row-user {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .user-image img {
+    border-radius: 2em;
+    width: 3em;
+    height: 3em;
+  }
+
+  .user-image {
+    padding-right: 1em;
+    display: ${(props) => (props.corLetra === "nulo" ? "none" : "block")};
+  }
+
+  @media (max-width: 425px) {
+    .pc {
+      display: none;
     }
 
     .cell {
-          display: none;
-      }
+      display: block;
+      flex-direction: row;
+      display: flex;
+      width: auto;
+      font-size: 0.8em;
+      align-items: center;
+      margin: 0em;
+      padding: 0.3em;
+      justify-content: space-around;
+    }
+
+    .carrinho {
+      width: auto;
+      height: 2em;
+      padding: 0em;
+      content: url("/assets/images/carrinho.svg");
+      margin-right: 1.3em;
+      display: ${(props) => (props.corLetra === "nulo" ? "block" : "block")};
+    }
 
     .entrega {
-        color: #ffffff;
-        font-size: 1.5em;
-        font-family: MontserratBold;
-        display: ${props => props.corLetra === 'branco'  ? 'block' : 'none' };
-        cursor: pointer;
+      display: ${(props) => (props.corLetra === "branco" ? "block" : "none")};
+      color: #f0ac54;
+    }
+
+    .column {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+
+    .agp-direita {
+      display: flex;
+      flex-direction: row;
+      align-self: center;
+      justify-content: space-around;
+    }
+    .lupa img {
+      height: 1.8em;
+      width: auto;
+    }
+    .login {
+      color: #f0ac54;
+      display: ${(props) => (props.corLetra === "nulo" ? "block" : "block")};
+    }
+
+    .logo-cabecalho {
+      padding-right: 3em;
     }
 
     hr {
-        margin-left: 3.4em;
-        margin-right: 3.4em;
-        border-color: ${props => props.corLetra === 'nulo' ? '#F0AC54' : '#FFFFFF'};
-        display: ${props => props.corLetra === 'branco' ? 'none' : 'block' };
+      width: 100%;
+      border-color: #f0ac54;
+      margin: 0em;
+      display: ${(props) => (props.corLetra === "branco" ? "block" : "block")};
+    }
+  }
+
+  @media (max-width: 1440px) {
+    .user-login {
+      font-size: 1.2em;
     }
 
-    .row-user {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
+    .entrega {
+      font-size: 1.4em;
     }
+  }
+`;
 
-    .user-image img {
-        border-radius: 2em;
-        width: 3em;
-        height: 3em;     
-    }
-
-    .user-image {
-        padding-right: 1em;
-        display: ${props => props.corLetra === 'nulo'? 'none' : 'block' };
-    }
-
-    @media (max-width: 425px) {
-
-        .pc {
-            display: none;
-        }
-
-      .cell {
-          display: block;
-          flex-direction: row;
-          display: flex;
-          width: auto;
-          font-size: 0.8em;
-          align-items: center;
-          margin: 0em;
-          padding: 0.3em;
-          justify-content: space-around;
-          
-        }
-
-       
-
-      .carrinho {
-          width: auto;
-          height: 2em;
-          padding: 0em;
-          content: url('/assets/images/carrinho.svg');
-          margin-right: 1.3em;
-          display: ${props => props.corLetra === 'nulo'  ? 'block' : 'block' };
-          
-      }
-
-      .entrega {
-          display: ${props => props.corLetra === 'branco' ? 'block' : 'none' };
-          color: #F0AC54;
-      }
-
-      .column {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-      }
-
-      .agp-direita {
-          display: flex;
-          flex-direction: row;
-          align-self: center;
-          justify-content: space-around;
-      }
-      .lupa img {
-        height: 1.8em;
-        width: auto;
-      }
-      .login {
-        color: #F0AC54;
-        display: ${props => props.corLetra === 'nulo'  ? 'block' : 'block' };
-      }
-
-     
-      .logo-cabecalho {
-        padding-right: 3em;
-      }
-
-      hr {
-          width: 100%;
-          border-color: #F0AC54;
-          margin: 0em;
-          display: ${props => props.corLetra === 'branco' ? 'block' : 'block' };
-      }
-    }
-
-    @media(max-width: 1440px) {
-        .user-login {
-            font-size: 1.2em;
-        }
-
-        .entrega {
-            font-size: 1.4em;
-        }
-    }
-
- 
-    
-`
-
-
-export { StyledCabecalho }
+export { StyledCabecalho };
