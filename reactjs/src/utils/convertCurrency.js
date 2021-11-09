9c42109ca3d7c5d1dd49c4811ea942143508c6d5
luigi <luigidasilvacoelho@gmail.com>
2021-11-09 09:48:43 -0300

    ok

---
+++ reactjs/src/utils/convertCurrency.js
@@ -0,0 +1,17 @@
+
+function convert(number){
+    let num = number.toString();
+    let conversion = num.replace(/[.]/, ",")
+    return conversion;
+}
+
+function reconvert(number){
+    let num = number.toString();
+    let conversion = num.replace(/[,]/, ".")
+    
+    
+    let r = Number(conversion)
+    return r;
+}
+
+export {convert, reconvert}
