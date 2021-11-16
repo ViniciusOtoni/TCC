af6dd3ebc5282764d7a7daafef8f9a0199dd458a
Vinícius <viniciusotonimsv@gmail.com>
2021-11-15 22:29:27 -0200

    OK

--- reactjs/src/services/api.js
+++ reactjs/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "https://gamebudheroko.herokuapp.com/",
+  baseURL: "https://gameheroko.herokuapp.com/",
 });
 
 export default class Api {
